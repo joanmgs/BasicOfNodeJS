c3b4c4824fb46c9c6e03cf5770263471cca62ec5
joanmgs <joan.gs@outlook.com>
2022-01-02 19:50:08 -0500

    Agregando index para levantar server

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+//Server function
+const handleServer = (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>You you...</h1>');
+    res.end();
+};
+
+//Creating server
+const server = http.createServer(handleServer);
+
+//lifting server in port 3030
+server.listen(3030, () => {
+    console.log('Working and running on 3030!');
+});
