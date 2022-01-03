12b727ac8675e8486b36d8628f9ceaf659c0d64e
joanmgs <joan.gs@outlook.com>
2022-01-02 19:59:20 -0500

    agregando colors

---
+++ README.md
@@ -0,0 +1,7 @@
+Creationg a Server with NodeJS in port 3030
+
+- Create an index and import http library of NodeJS
+- Create a function for the server, and the server
+- Lift the server in port 3030
+
+- Used colors library for change the colors of the text in the console
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const colors = require('colors');
 
 //Server function
 const handleServer = (req, res) => {
@@ -12,5 +13,5 @@ const server = http.createServer(handleServer);
 
 //lifting server in port 3030
 server.listen(3030, () => {
-    console.log('Working and running on 3030!');
+    console.log('Working and running on 3030!'.green);
 });
